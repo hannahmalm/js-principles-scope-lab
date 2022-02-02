a3dd06ad3dda14fc231226967019a7e772237e52
Hannah Malm <hannahemalm616@gmail.com>
2022-02-01 18:51:35 -0600

    commit tree ed5fe31612fe3aa5028a6d4d1a57ff178fa9ced5

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+const leastFavoriteCustomer = 'its you'
+
+function upperCaseCustomerName(customerName) {
+ const customerName =  customerName.toUpperCase()
+//    console.log(upper);
+
+}
+
+function setBestCustomer(){
+     bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'tom'
+}
